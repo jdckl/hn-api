7cdd7f42c151acb0ca58f7d94583cf2ea521b671
Jan Dočekal <hello@jdckl.dev>
2022-01-17 12:39:34 +0100

    Implement base HN api structure, tests

---
+++ src/__tests__/hn.test.ts
@@ -0,0 +1,13 @@
+import { getItemById } from '../hacker-news/hackerNewsApi';
+
+// Single item
+it('should get a single HN item from the API', async () => {
+    const hnItem = await getItemById('29965110');
+    expect(hnItem?.title).toBe('The curious case of the Raspberry Pi in the network closet (2019)');
+})
+
+// No item found
+it('should return an undefined item from the HN API', async () => {
+    const hnItem = await getItemById('x');
+    expect(hnItem).toBeUndefined();
+})
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import 'dotenv/config';
+import express from 'express';
+
+const APP = express();
